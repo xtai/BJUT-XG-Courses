4be3bf888a3aff85187b19ef018f84d48a000cbc
Xiaoyu Tai <taixiaoyu@gmail.com>
2014-04-11 21:49:45 +0800

    add setting example file

---
+++ app/setting-example.php
@@ -0,0 +1,9 @@
+<?php
+  $include_path = "";        //--> 'app' folder base path e.g. .:/var/www/xg/app
+  $path = "";                //--> website base path e.g. xg.taixiaoyu.com
+
+  $db_location = "server";   //--> database settings
+  $db_username = "username"; //^
+  $db_password = "password"; //^
+  $db_database = "database"; //^
+?>
