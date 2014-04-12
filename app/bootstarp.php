13147fd430ba11f2a4260fadae275665062d1a9b
Xiaoyu Tai <taixiaoyu@gmail.com>
2014-04-12 21:40:38 +0800

    testing github connection

--- app/bootstarp.php
+++ app/bootstarp.php
@@ -6,18 +6,19 @@
 
   //!IMPORTANT: see setting_example.php
   include_once "setting.php";
-  ini_set("include_path", $include_path); 
 
   //Import All Classes
+  ini_set("include_path", $include_path); 
+  
   include_once "package/mysql.php";
   include_once "package/user.php";
   include_once "package/subjects.php";
   include_once "package/view.php";
+
   session_start();
 
   $MySQL   = new MySQL($db_location, $db_username, $db_password, $db_database);
   $Subject = new Subject($MySQL, $path);
   $User    = new User($MySQL);
   $View    = new View($MySQL, $User, $Subject, $path);
-
 ?>
