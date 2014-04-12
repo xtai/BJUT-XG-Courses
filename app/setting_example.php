809c376f07e5b3fd5e00fc588a62c9f191b6cd8b
Xiaoyu Tai <taixiaoyu@gmail.com>
2014-04-12 12:02:12 +0800

    :D

--- app/setting_example.php
+++ app/setting_example.php
@@ -3,13 +3,13 @@
   * XG-Courses Setting Example
   * by Xiaoyu Tai @ Beijing, 2014.4.12
   */
-  $include_path = "";        //--> 'app' folder base path e.g. .:/var/www/xg/app
-  $path = "";                //--> website base path e.g. xg.taixiaoyu.com
+  $include_path     = "";         //--> 'app' folder base path e.g. .:/var/www/xg/app
+  $path             = "";         //--> website base path e.g. xg.taixiaoyu.com
 
-  $db_location = "server";   //--> database settings
-  $db_username = "username"; //^
-  $db_password = "password"; //^
-  $db_database = "database"; //^
+  $db_location      = "server";   //--> database settings
+  $db_username      = "username"; //^
+  $db_password      = "password"; //^
+  $db_database      = "database"; //^
 
-  $google_analytics = "";    //--> Google Analytics Tracking Code
+  $google_analytics = "";         //--> Google Analytics Tracking Code
 ?>
