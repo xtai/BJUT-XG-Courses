91b74237a1cf79dd60933a382bcec3b2327ae253
Xiaoyu Tai <taixiaoyu@gmail.com>
2014-04-26 20:24:57 +0800

    .gitignore...

--- app/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $path             = "http://".$_SERVER['SERVER_NAME'];
-  $base_file_path   = __DIR__;
-  $db_location      = "localhost";
-  $db_username      = "tai";
-  $db_password      = "12345678";
-  $db_database      = "xg_courses";
-  $google_analytics = "UA-47362352-2";
-?>
