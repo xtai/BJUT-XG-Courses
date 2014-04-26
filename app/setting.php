6dd308f37caa12cffc2a07cb8816b09ed67d24f6
Xiaoyu Tai <taixiaoyu@gmail.com>
2014-04-26 20:13:19 +0800

    rename Moudle

---
+++ app/setting.php
@@ -0,0 +1,9 @@
+<?php
+  $include_path     = ".:/var/www/xg/app/";
+  $path             = "http://xg.taixiaoyu.com";
+  $db_location      = "localhost";
+  $db_username      = "tai";
+  $db_password      = "12345678";
+  $db_database      = "xg_courses";
+  $google_analytics = "UA-47362352-2";
+?>
